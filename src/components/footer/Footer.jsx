120184bea9a54c9a45daa5b7267701e994606cfe
Likhon Islam <md.likhonislam2x@gmail.com>
2023-10-26 11:30:39 +0600

    first commit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -115,7 +115,7 @@ function Footer() {
                   </div>
                   <div className="col-span-6 flex flex-col items-center md:items-start gap-y-4">
                     <span className="text-[12px] text-[#979797]">CONNECT</span>
-                    <NavHashLink to="press#git commit -m "first commit"">
+                    <NavHashLink to="press#">
                       <button className="text-[20px] lg:text-[24px] text-[#FCFCFC] leading-[32px] font-light hover:faded-lilac">
                         Press
                       </button>
